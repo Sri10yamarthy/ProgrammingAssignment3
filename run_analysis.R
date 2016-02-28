171dc741d23bf81921780affe898aa83aae7d05c
sri10yamarthy <sri10yamarthy@gmail.com>
2016-02-29 04:45:06 +0530

    assign3

--- run_analysis.R
+++ run_analysis.R
@@ -36,6 +36,6 @@ run_analysis<-function(dir) {
     mn_list<-grep("mean",clmn_name)
     final_list<-c(1,mn_list,ncol(rd21))
     final_table1<-rd21[,final_list]
-    write.table(final_table1,file = wrt_dir)
+    write.table(final_table1,file = wrt_dir,row.names = FALSE)
 }
     
